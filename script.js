d53faf3ec65442316a05b625049e97414186ca04
arif <arifprog1@gmail.com>
2024-12-09 17:26:09 +0000

    Eight solution

--- script.js
+++ script.js
@@ -58,3 +58,16 @@ const person = {
 alert(person.name+ " is " +person.age);
 
 
+
+//Alert the number of items in an array, using the correct Array property: 
+
+const cars = ["Volvo", "Jeep", "Mercedes"];
+alert(cars.length);
+
+
+//Change the first item of Brand to "Ford".
+
+const Brand = ["Volvo", "Jeep", "Mercedes"];
+Brand[0]="Ford";
+console.log(Brand);
+ 
