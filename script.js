f09c3e0a495d54bf6e38435201bde69436c95cdb
arif <arifprog1@gmail.com>
2024-12-09 10:58:57 +0000

    Second solution

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 //Create a variable called carName, assign the value Volvo to it.
 
 let carName = "Volvo";
-console.log(carName);
+document.write(carName);
+
+//On one single line, declare three variables with the following names and values:
+
+//lastNamee = "John"
+//lastName = "Doe"
+//age = 35
+
+/*let firstName = "John";
+let lastName = "Doe";
+let age = 35;*/
+
+
+let firstName = "John", lastName = "Doe", age = 35;
+
+document.write(firstName,lastName,age);
+
