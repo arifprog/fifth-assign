b2c29b5eedd413b6b2ca03db57e107419c7d1468
arif <arifprog1@gmail.com>
2024-12-11 17:57:26 +0000

    Thirteenth solution

--- script.js
+++ script.js
@@ -135,6 +135,22 @@ console.log(isEven(7)); // false
 
 
 
+//Write a function to sum two numbers. The function should take two arguments, which are the numbers to sum.
+  
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(sum(3, 4)); // 7
+console.log(sum(10, 20)); // 30
+
+
+
+
+
+
+
+
 
 
 
