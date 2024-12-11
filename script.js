63f67f6aac6500653223deebee40f85c231008f5
arif <arifprog1@gmail.com>
2024-12-11 17:30:59 +0000

    Eleventh solution

--- script.js
+++ script.js
@@ -111,4 +111,18 @@ alert(ageTwo < 18 ? "Too young" : "Old enough");
 
 
 
+//Write a function to convert Celsius to Fahrenheit. The function should take a single argument, which is the temperature in Celsius.
+
+  
+function celsiusToFahrenheit(celsius) {
+  return (celsius * 9/5) + 32;
+}
+
+console.log(celsiusToFahrenheit(0)); // 32
+console.log(celsiusToFahrenheit(25)); // 77
+
+
+
+
+
 
