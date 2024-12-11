ed313690f1266fc4756fbd3a62fc970c383de8b7
arif <arifprog1@gmail.com>
2024-12-11 17:42:13 +0000

    Twelve solution

--- script.js
+++ script.js
@@ -123,6 +123,23 @@ console.log(celsiusToFahrenheit(25)); // 77
 
 
 
+//Write a function to check if a number is even. The function should take a single argument, which is the number to check
+
+
+function isEven(num) {
+  return num % 2 === 0;
+}
+
+console.log(isEven(4)); // true
+console.log(isEven(7)); // false
+
+
+
+
+
+
+
+
 
 
 
