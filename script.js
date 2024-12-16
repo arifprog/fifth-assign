ff955692ebd18ffd4db1f5c2f8c4b8a09ebf3ddb
arif <arifprog1@gmail.com>
2024-12-16 23:10:18 +0000

    Seventeenth solution

--- script.js
+++ script.js
@@ -197,6 +197,17 @@ console.log(getFirstElement(["a", "b", "c"])); // "a"
 
 
 
+//Write a function to check if an array is empty. The function should take a single argument, which is the array.
+  
+function isArrayEmpty(arr) {
+  return arr.length === 0;
+}
+
+console.log(isArrayEmpty([])); // true
+console.log(isArrayEmpty([1, 2, 3])); // false
+
+
+
 
 
 
