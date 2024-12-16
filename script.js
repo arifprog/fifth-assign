e47f9b1a2bf2eb11a4a89b7c502f7ad3a04317ba
arif <arifprog1@gmail.com>
2024-12-16 23:37:09 +0000

    Nineteenth solution

--- script.js
+++ script.js
@@ -222,6 +222,15 @@ console.log(factorialize(7)); // 5040
 
 
 
+//Write a function to reverse a string. The function should take a single argument, which is the string to reverse.
+
+  
+function reverseString(str) {
+  return str.split(''). reverse().join('');
+}
+
+console.log(reverseString("hello")); // "olleh"
+console.log(reverseString("world")); // "dlrow"
 
 
 
