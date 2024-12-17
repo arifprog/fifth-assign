81cd190315d8d3f1a2172feab57e587f8fa7c937
arif <arifprog1@gmail.com>
2024-12-17 00:45:50 +0000

    Twenty five solution

--- script.js
+++ script.js
@@ -288,3 +288,20 @@ function getFirstCharacter(str) {
 
 console.log(getFirstCharacter("hello")); // "h"
 console.log(getFirstCharacter("world")); // "w"
+
+
+
+//Write a function to find the sum of all elements in an array. The function should take a single argument, which is the array.
+
+  
+function sumArray(arr) {
+let sum = 0;
+  for (let i = 0; i < arr.length;i++){
+    sum+= arr[i];
+}
+  return sum;
+}
+
+console.log(sumArray([1, 2, 3, 4])); // 10
+console.log(sumArray([-1, -2, -3, -4])); // -10
+console.log(sumArray([1.5, 2.5, 3.5])); // 7.5
