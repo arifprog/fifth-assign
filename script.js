46d4973a81118ee8695521e55f158da2ae9567c1
arif <arifprog1@gmail.com>
2024-12-17 00:03:49 +0000

    Twenty solution

--- script.js
+++ script.js
@@ -234,6 +234,14 @@ console.log(reverseString("world")); // "dlrow"
 
 
 
+//Write a function to convert a string to lowercase. The function should take a single argument, which is the string to convert.
+  
+function toLowerCase(str) {
+  return str.toLowerCase();
+}
+
+console.log(toLowerCase("HELLO WORLD")); // "hello world"
+console.log(toLowerCase("JavaScript")); // "javascript"
 
 
 
