cf444a275eabedf970c70646ce68528510d59c48
arif <arifprog1@gmail.com>
2024-12-17 00:21:14 +0000

    Twenty Three solution

--- script.js
+++ script.js
@@ -268,3 +268,12 @@ console.log(mergeArrays([1, 2, 3], [4, 5, 6])); // [1, 2, 3, 4, 5, 6]
 console.log(mergeArrays(["a", "b"], ["c", "d"])); // ["a", "b", "c", "d"]
 
 
+//Write a function to get the last element of an array. The function should take a single argument, which is the array.
+
+  
+function getLastElement(arr) {
+  return arr[arr.length-1];
+}
+
+console.log(getLastElement([1, 2, 3])); // 3
+console.log(getLastElement(["a", "b", "c"])); // "c"
