def185db268794bd39bfd911e7afd546fd12a7a1
arif <arifprog1@gmail.com>
2024-12-17 00:08:34 +0000

    Twenty one solution

--- script.js
+++ script.js
@@ -246,6 +246,14 @@ console.log(toLowerCase("JavaScript")); // "javascript"
 
 
 
+//Write a function to find the length of a string. The function should take a single argument, which is the string.
+  
+function stringLength(str) {
+  return str.length;
+}
+
+console.log(stringLength("hello")); // 5
+console.log(stringLength("world")); // 5
 
 
 
